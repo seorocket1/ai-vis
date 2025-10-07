bb8063b3eb4d0f474a6056a5f82b188eec75220c
seorocket1 <seorocket001@gmail.com>
2025-10-07 14:03:35 +0530

    Updated config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,4 +1,4 @@
 export const config = {
-  supabaseUrl: 'https://0ec90b57d6e95fcbda19832f.supabase.co',
-  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJib2x0IiwicmVmIjoiMGVjOTBiNTdkNmU5NWZjYmRhMTk4MzJmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg4ODE1NzQsImV4cCI6MTc1ODg4MTU3NH0.9I8-U0x86Ak8t2DGaIk0HfvTSLsAyzdnz-Nw00mMkKw',
+  supabaseUrl: 'https://ypztlmwevcqqcfbzzzsa.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlwenRsbXdldmNxcWNmYnp6enNhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkyOTUxNDcsImV4cCI6MjA3NDg3MTE0N30.gEoE22qbE4QevfZ8Td4wTeDRxMjfnZwzrTHin3lTMUk',
 };
